c82827b67e46d076cab1622c9fb3d4a063c179c4
Fabien <jouanneau.fab@gmail.com>
2020-07-22 10:06:41 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SMC-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sharemycar';
+}
