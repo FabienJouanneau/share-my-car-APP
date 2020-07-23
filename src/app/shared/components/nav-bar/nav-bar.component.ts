7c1dc990412eb42e6b9a777358214b21d2472595
Fabien <jouanneau.fab@gmail.com>
2020-07-24 00:00:35 +0200

    feat (nav-bar): ajout bouton déconnexion et méthodes

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'SMC-nav-bar',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  displayDeconnexionButton: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.connexionTest();
+    }, 200);
+  }
+  connexionTest(){
+    if (localStorage.getItem('token')){
+      this.displayDeconnexionButton = true;
+    } else {
+      this.displayDeconnexionButton = false;
+    }
+  }
+  deconnectUser(){
+    this.authService.removeToken();
+    this.displayDeconnexionButton = false;
+  }
+  navigate(){
+    const userId = localStorage.getItem('userId');
+    if (userId){
+      this.router.navigateByUrl(`/profile/${userId}`);
+    } else {
+      this.router.navigateByUrl('/auth');
+    }
   }
 }
 
