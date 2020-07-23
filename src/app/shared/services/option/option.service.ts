066afd60973687eb17f180ec8817299c14587e59
Fabien <jouanneau.fab@gmail.com>
2020-07-24 00:03:54 +0200

    modif méthodes

--- src/app/shared/services/option/option.service.ts
+++ src/app/shared/services/option/option.service.ts
@@ -8,22 +8,25 @@ import { environment } from '../../../../environments/environment';
   providedIn: 'root'
 })
 export class OptionService {
-  private static OPTION_URL = environment.baseUrl + 'users';
+  private static OPTION_URL = environment.baseUrl + 'options';
 
   constructor(
     private http: HttpClient,
   ) { }
 
-  getOptionById(id: number): Observable < Option > {
+  getAllOptions(): Observable<Option[]> {
+    return this.http.get<Option[]>(`${OptionService.OPTION_URL}`);
+  }
+  getOptionById(id: number): Observable<Option> {
     return this.http.get<Option>(`${OptionService.OPTION_URL}/${id}`);
   }
-  postOptionByid(option: Option): Observable < Option > {
+  postOptionByid(option: Option): Observable<Option> {
     return this.http.post<Option>(`${OptionService.OPTION_URL}`, option);
   }
-  putOptionByid(option: Option): Observable < Option > {
-    return this.http.put<Option>(`${OptionService.OPTION_URL}/${option.id}`, Option);
+  putOptionByid(option: Option): Observable<Option> {
+    return this.http.put<Option>(`${OptionService.OPTION_URL}/${option.carOptionId}`, Option);
   }
-  deleteOptionByid(option: Option): Observable < void > {
-    return this.http.delete<void>(`${OptionService.OPTION_URL}/${option.id}`);
+  deleteOptionByid(option: Option): Observable<void> {
+    return this.http.delete<void>(`${OptionService.OPTION_URL}/${option.carOptionId}`);
   }
 }
