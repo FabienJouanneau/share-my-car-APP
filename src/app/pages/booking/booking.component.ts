18021157bea8c6d07ae060d05108ce71cc90b30f
Fabien <jouanneau.fab@gmail.com>
2020-07-23 23:58:24 +0200

    feat (booking): Creation

---
+++ src/app/pages/booking/booking.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Booking } from '../../shared/classes/booking';
+import { Advert } from '../../shared/classes/advert';
+import { BookingService } from '../../shared/services/booking/booking.service';
+import { User } from '../../shared/classes/user';
+
+@Component({
+  selector: 'SMC-booking',
+  templateUrl: './booking.component.html',
+  styleUrls: ['./booking.component.scss']
+})
+export class BookingComponent implements OnInit {
+  reservation: Booking = {startingDate: null, endingDate: null, amount: 0};
+  daysCounter = 0;
+  dateNow = new Date(Date.now());
+  @Input() advert: Advert;
+  @Input() user: User;
+  @Output() postReservation = new EventEmitter();
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+    this.reservation.advert = {advertId: this.advert.advertId};
+    this.reservation.user = {userId: this.user.userId};
+  }
+  calculatePrice(){
+    const a = +new Date(this.reservation.startingDate);
+    const b = +new Date(this.reservation.endingDate);
+    const c = +this.dateNow - (1 * 1000 * 60 * 60 * 24) ;
+    if (a <= b && a > c && b > c){
+      this.daysCounter = (b - a) / (1000 * 60 * 60 * 24) + 1;
+      return this.reservation.amount = this.daysCounter * this.advert.carPricePerDay;
+    } else {
+      this.daysCounter = 0;
+      this.reservation.amount = 0;
+    }
+  }
+  onSubmit(){
+    this.postReservation.emit(this.reservation);
+    console.log(this.reservation);
+  }
+}
