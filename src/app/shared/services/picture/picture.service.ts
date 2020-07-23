47bd1c922316e8bdffe7ec1ee0b3531be9ddadf1
Fabien <jouanneau.fab@gmail.com>
2020-07-23 02:13:12 +0200

    feat (picture.service): service creation

---
+++ src/app/shared/services/picture/picture.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Picture } from '../../classes/picture';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PictureService {
+  private static PICTURE_URL = environment.baseUrl + 'users';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getPictureById(id: number): Observable < Picture > {
+    return this.http.get<Picture>(`${PictureService.PICTURE_URL}/${id}`);
+  }
+  postPictureByid(picture: Picture): Observable < Picture > {
+    return this.http.post<Picture>(`${PictureService.PICTURE_URL}`, picture);
+  }
+  putPictureByid(picture: Picture): Observable < Picture > {
+    return this.http.put<Picture>(`${PictureService.PICTURE_URL}/${picture.id}`, Picture);
+  }
+  deletePictureByid(picture: Picture): Observable < void > {
+    return this.http.delete<void>(`${PictureService.PICTURE_URL}/${picture.id}`);
+  }
+}
